3682f263a9f880b3c33ff33d758be1c90d05fafb
JessicaEverington <jessica.everington@gmail.com>
2020-03-03 01:57:53 -0800

    creating new movie app

---
+++ src/components/Text1.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Text1 extends Component {
+    render() {
+        return(
+            <p>Hello from text1 -testing how to insert class-based component</p>
+        )
+    }
+}
+
+export default Text1;
